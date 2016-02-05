07a7df5e00fa95d61d88b4ef23751ef887f8b9e6
haitike <haitike@gmail.com>
2016-02-05 14:17:42 +0100

    Improving i18n

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Python Telegram webhook bot.
 A simple Telegram bot written in Python using the webhook API.
+
+Instructions:
+- Create the file data/token and add your token there.
--- temporal-nowebhook-bot.py
+++ temporal-nowebhook-bot.py
@@ -1,17 +1,54 @@
 from telegram import Updater
 
 import gettext
+import configparser
+
 locale_path = "locale"
+data_path = "data/"
+language_list_path= "locale/language_list"
+default_language = "en" # This is only used the first time, when data/config doesn't exist. 
+current_language = ""
 
-translate_en = gettext.translation("telegrambot", locale_path, languages=['en'])
-translate_es = gettext.translation("telegrambot", locale_path, languages=['es'])
-translate_fl = gettext.translation("telegrambot", locale_path, languages=['fl'])
+languages = {} # Key = ISO Code, Value = Language name
+translate = {} # Key = ISO Code, Value = Language Translation
 
-translate_en.install()
+# FILE STUFF (data/token  &  locale/language_list)
 
-token_path = 'data/api.token'
-f = open(token_path)
+f = open(data_path+"token", "r")
 token = (f.read().strip())
+f.close()
+
+f = open(language_list_path,"r")
+language_list = f.read().splitlines()
+f.close()
+
+for line in language_list:
+    l = line.split()
+    languages[l[0]] = l[1]
+
+for l in languages:
+    translate[l] = gettext.translation("telegrambot", locale_path, languages=[l]) 
+
+# CONFIG FILE STUFF
+
+config = configparser.ConfigParser()
+config.read('data/config')
+if config.has_section("bot"):
+    if config.has_option("bot","language"):
+        config_language = config["bot"]["language"]
+        if config_language in languages:
+            current_language = config_language
+        else:
+            current_language = default_language
+    else:
+        current_language = default_language
+else:
+    current_language = default_language
+
+# Set the language of messages
+translate[current_language].install()
+
+# FUNTIONS
 
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text=_("Bot was initiated. Use /help for commands."))
@@ -36,26 +73,25 @@ def search(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text=_("/search <engine> <word>"))
 
 def settings(bot,update):
-    help_text = _("Use /settings <language/l> <es/en/fl>")
+    lcodelist_text = _("Language codes:\n")
+    for l in languages:
+        lcodelist_text+= l + " - " + languages[l] + "\n"
+
+    help_text = _("Use /settings language <language_code>\n\n" + lcodelist_text)
+
     command_args = update.message.text.split()
     if len(command_args) < 3:
         bot.sendMessage(chat_id=update.message.chat_id, text=help_text)
     else:
         if command_args[1] == "language" or "l":
-            if command_args[2] == "en":
-                bot.sendMessage(chat_id=update.message.chat_id, text=_("Language changed to English"))
-                translate_en.install()
-            elif command_args[2] == "es":
-                bot.sendMessage(chat_id=update.message.chat_id, text=_("Language changed to Spanish"))
-                translate_es.install()
-            elif command_args[2] == "fl":
-                bot.sendMessage(chat_id=update.message.chat_id, text=_("Language changed to Flavoured"))
-                translate_fl.install()
+            if command_args[2] in languages:
+                bot.sendMessage(chat_id=update.message.chat_id, text=_("Language changed to %s") % (languages[command_args[2]]) )
+                current_language =  command_args[2]
+                translate[current_language].install()
             else:
-                bot.sendMessage(chat_id=update.message.chat_id, text=_("Unknown language code"))
+                bot.sendMessage(chat_id=update.message.chat_id, text=_("Unknown language code\n\n" + lcodelist_text))
         else:
             bot.sendMessage(chat_id=update.message.chat_id, text=help_text)
-
 def unknown(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text=_("%s is a unknown command. Use /help for available commands.") % (update.message.text))
 
@@ -77,6 +113,7 @@ def main():
 
     updater.idle()
 
-if __name__ == '__main__':
+    # SAVE CONFIG FILE HERE
+if __name__ == "__main__":
     main()
 
