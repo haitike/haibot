79fb5b429bc6237b6717f97b78a7fbbc226cc52f
haitike <haitike@gmail.com>
2016-02-05 02:59:18 +0100

    i18n (English, Spanish, "Flavoured") + Command Reestructuration

--- temporal-nowebhook-bot.py
+++ temporal-nowebhook-bot.py
@@ -1,60 +1,74 @@
 from telegram import Updater
 
+import gettext
+locale_path = "locale"
+
+translate_en = gettext.translation("telegrambot", locale_path, languages=['en'])
+translate_es = gettext.translation("telegrambot", locale_path, languages=['es'])
+translate_fl = gettext.translation("telegrambot", locale_path, languages=['fl'])
+
+translate_en.install()
+
 token_path = 'data/api.token'
 f = open(token_path)
 token = (f.read().strip())
 
 def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text='The end is near. EXTERMINATE! EXTERMINATE!')
+    bot.sendMessage(chat_id=update.message.chat_id, text=_("Bot was initiated. Use /help for commands."))
 
 def help(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text='No me cuentes tu vida, no soy tu chacha!')
+    bot.sendMessage(chat_id=update.message.chat_id, text=_("""Available Commands:
+/start - Iniciciate or Restart the bot
+/help - Show the command list.
+/terraria status/autonot/ip - Terraria options
+/list <option> <item> - Manage your lists.
+/search <engine> <word> - Search using a engine."""))
 
 def terraria(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="""/terraria status
+    bot.sendMessage(chat_id=update.message.chat_id, text=_("""/terraria status
 /terraria autonot
-/terraria ip""")
+/terraria ip"""))
 
-def lista(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="""/lista create_list
-/lista remove_list
-/lista add
-/lista remove
-/lista print
-/lista print_all
-/lista done""")
+def list(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text=_("/list <option> <item>"))
 
-def quote(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="""/quote <number>
-/quote search
-/quote shortlist
-/quote add
-/quote remove""")
+def search(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text=_("/search <engine> <word>"))
 
-def etimologia(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Indica una palabra a buscar")
-
-def evolve(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Indica una feature para implementar")
-
-def levelup(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="PLACEHOLDER")
+def settings(bot,update):
+    help_text = _("Use /settings <language/l> <es/en/fl>")
+    command_args = update.message.text.split()
+    if len(command_args) < 3:
+        bot.sendMessage(chat_id=update.message.chat_id, text=help_text)
+    else:
+        if command_args[1] == "language" or "l":
+            if command_args[2] == "en":
+                bot.sendMessage(chat_id=update.message.chat_id, text=_("Language changed to English"))
+                translate_en.install()
+            elif command_args[2] == "es":
+                bot.sendMessage(chat_id=update.message.chat_id, text=_("Language changed to Spanish"))
+                translate_es.install()
+            elif command_args[2] == "fl":
+                bot.sendMessage(chat_id=update.message.chat_id, text=_("Language changed to Flavoured"))
+                translate_fl.install()
+            else:
+                bot.sendMessage(chat_id=update.message.chat_id, text=_("Unknown language code"))
+        else:
+            bot.sendMessage(chat_id=update.message.chat_id, text=help_text)
 
 def unknown(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text='Ese comando no existe. Usa /help [Es una sugerencia, no te ofendas conmigo :(  ] ')
+    bot.sendMessage(chat_id=update.message.chat_id, text=_("%s is a unknown command. Use /help for available commands.") % (update.message.text))
 
 def main():
     updater = Updater(token=token)
     dp = updater.dispatcher
 
-    dp.addTelegramCommandHandler('start', start)
-    dp.addTelegramCommandHandler('help', help)
-    dp.addTelegramCommandHandler('terraria', terraria)
-    dp.addTelegramCommandHandler('lista', lista)
-    dp.addTelegramCommandHandler('quote', lista)
-    dp.addTelegramCommandHandler('etimologia', lista)
-    dp.addTelegramCommandHandler('evolve', lista)
-    dp.addTelegramCommandHandler('levelup', lista)
+    dp.addTelegramCommandHandler("start", start)
+    dp.addTelegramCommandHandler("help", help)
+    dp.addTelegramCommandHandler("terraria", terraria)
+    dp.addTelegramCommandHandler("list", list)
+    dp.addTelegramCommandHandler("search", search)
+    dp.addTelegramCommandHandler("settings",settings)
     dp.addUnknownTelegramCommandHandler(unknown)
 
     #dp.addErrorHandler(error)
