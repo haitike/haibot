d7cfb280d03c6bf034022f72b835f5b87193d0b5
haitike <haitike@gmail.com>
2016-02-13 23:08:13 +0100

    fixing webhook error.

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 A simple Telegram bot written in Python using the webhook API.
 
 Instructions:
-- Edit data/config.cfg and add your bot token to the token key.
-- If you want to use a webhook, in the same file you can add your web url to webhook_url. 
-- Use webhook_main.py or polling_main.py (if you don't want to use a webhook). You can enable or disable webhook with the scripts in tools/ 
+- Raname data/config-default.py to data/config.py.
+- Fill the token value. If you are going to use a webhook fill the webhook_url too.
+- The default config is for openshift. Modify it for other hosting.
+- Use webhook_main.py or polling_main.py (if you don't want to use a webhook). You can enable or disable webhook with the scripts in tools/
--- webhook_main.py
+++ webhook_main.py
@@ -7,20 +7,19 @@ CONFIGFILE_PATH = joinpath(DATA_PATH, "config.py")
 
 app = Flask(__name__)
 app.config.from_pyfile(CONFIGFILE_PATH) #try
+bot = telegram.Bot(token=app.config["TOKEN"])  #try
 
 @app.route("/")
 def test():
     return "<strong>It's Alive!</strong>"
 
-@app.route('/'+app.config["TOKEN"])
+@app.route('/'+app.config["TOKEN"], methods=['POST'])
 def webhook_handler():
     update = telegram.Update.de_json(request.get_json(force=True))
     chat_id = update.message.chat.id
     text = update.message.text.encode('utf-8')
-    bot.sendMessage(chat_id=chat_id, text=text) # echo (temp)
-
+    bot.sendMessage(chat_id=chat_id, text=text)
     return 'ok'
 
 if __name__ == '__main__':
-    bot = telegram.Bot(token=app.config["TOKEN"])  #try
     app.run()
