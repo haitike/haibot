45412094cb13831eca9d01aa1c9e7c2264952f8a
haitike <haitike@gmail.com>
2016-02-16 18:34:14 +0100

    TelegramBot Class finished / Fixing a Unicode error in Python 2.X gettext module.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# Python Telegram webhook bot.
-A simple Telegram bot written in Python using the webhook API.
+A simple Telegram bot written in Python with some useful commands. You can run the bot using a polling loop, or with a webhook and Flask.
+
+Compability:
+- Python 2
+- Python 3
 
 Instructions:
 - Raname data/config-default.py to data/config.py.
-- Fill the token value. If you are going to use a webhook fill the webhook_url too.
-- The default config is for openshift. Modify it for other hosting.
-- Use webhook_main.py or polling_main.py (if you don't want to use a webhook). You can enable or disable webhook with the scripts in tools/
+- Fill the token value (talk to BodFather in telegram). If you are going to use a webhook fill the webhook_url too.
+- Use webhook_main.py or polling_main.py as you prefer.
+- Note: the flask config in config.py is prepared for OpenShift.
--- telegrambot/temp_telegrambot.py
+++ telegrambot/temp_telegrambot.py
@@ -2,124 +2,96 @@ from telegram import Updater, Dispatcher
 from os.path import join as joinpath
 import data.config as config
 
+
 import gettext
-import os
+import os, sys
 import logging
 
 LOCALE_PATH = "locale"
-DEFAULT_LANGUAGE = "en_EN" # This is only used the first time, when data/config doesn't exist.
-
-# Fix Python 2.x. input
-try: input = raw_input
-except NameError: pass
-
-translate = {}
-language_list = os.listdir(LOCALE_PATH)
-for l in language_list:
-    translate[l] = gettext.translation("telegrambot", LOCALE_PATH, languages=[l], fallback=True)
-
-current_language = DEFAULT_LANGUAGE
-############################ try:
-#if config.has_option("bot","language"):
-#    if config.get("bot","language") in language_list:
-#        current_language = config.get("bot","language")
-translate[current_language].install()
-
+DEFAULT_LANGUAGE = "en_EN"
 logger = logging.getLogger("bot_log")
 
-def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text=_("Bot was initiated. Use /help for commands."))
-
-def help(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text=_("""Available Commands:
-/start - Iniciciate or Restart the bot
-/help - Show the command list.
-/terraria status/autonot/ip - Terraria options
-/list <option> <item> - Manage your lists.
-/search <engine> <word> - Search using a engine."""))
-
-def terraria(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text=_("""/terraria status
-/terraria autonot
-/terraria ip"""))
-
-def list(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text=_("/list <option> <item>"))
-
-def search(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text=_("/search <engine> <word>"))
-
-def settings(bot,update):
-    languages_codes_text = _("Language codes:\n")
-    for lang in language_list:
-        languages_codes_text+= lang + " "
-
-    help_text = _("Use /settings language <language_code>\n\n" + languages_codes_text)
-
-    command_args = update.message.text.split()
-    if len(command_args) < 3:
-        bot.sendMessage(chat_id=update.message.chat_id, text=help_text)
-    else:
-        if command_args[1] == "language" or "l":
-            if command_args[2] in language_list:
-                bot.sendMessage(chat_id=update.message.chat_id, text=_("Language changed to %s") % (command_args[2]))
-                global current_language
-                current_language =  command_args[2]
-                translate[current_language].install()
-
-            else:
-                bot.sendMessage(chat_id=update.message.chat_id, text=_("Unknown language code\n\n" + languages_codes_text))
-        else:
-            bot.sendMessage(chat_id=update.message.chat_id, text=help_text)
-
-def unknown(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text=_("%s is a unknown command. Use /help for available commands.") % (update.message.text))
-
-def main():
-    updater = Updater(token=token)
-    dp = updater.dispatcher
-
-    dp.addTelegramCommandHandler("start", start)
-    dp.addTelegramCommandHandler("help", help)
-    dp.addTelegramCommandHandler("terraria", terraria)
-    dp.addTelegramCommandHandler("list", list)
-    dp.addTelegramCommandHandler("search", search)
-    dp.addTelegramCommandHandler("settings",settings)
-    dp.addUnknownTelegramCommandHandler(unknown)
-    #dp.addErrorHandler(error)
-
-    update_queue = updater.start_polling()
-    while True:
-        text = input("Write {stop} for stopping the bot\n")
-        if text == 'stop':
-            updater.stop()
-            break
-        elif len(text) > 0:
-            update_queue.put(text)  # Put command into queue
-
-    ############################ try:
-    config.set('bot', 'language', current_language)
-    with open(configfile_path, 'w') as configfile:    # save
-        config.write(configfile)
+def translation_install(translation): # Comnpability with both python 2 / 3
+    kwargs = {}
+    if sys.version < '3':
+        kwargs['unicode'] = True
+    translation.install(**kwargs)
 
 class TelegramBot(object):
+    translations = {}
+    api = None
+
     def __init__(self):
+
+        self.language_list = os.listdir(LOCALE_PATH)
+        for l in self.language_list:
+            self.translations[l] = gettext.translation("telegrambot", LOCALE_PATH, languages=[l], fallback=True)
+        try:
+            if config.LANGUAGE in self.language_list:
+                translation_install(self.translations[config.LANGUAGE])
+            else:
+                translation_install(self.translations[DEFAULT_LANGUAGE])
+        except:
+            translation_install(self.translations[DEFAULT_LANGUAGE])
+
+        #TEMPORAL
+        from telegram import Bot
+        self.api = Bot(token=config.TOKEN)  #try
+
         logger.debug("TelegramBot initialized")
 
+    def set_webhook(self):
+        s = self.api.setWebhook(config.WEBHOOK_URL + "/" + config.TOKEN)
+        if s:
+            logger.info("webhook setup worked")
+        else:
+            logger.warning("webhook setup failed")
+
+    def disable_webhook(self):
+        s = self.api.setWebhook("")
+        if s:
+            logger.info("webhook was disabled")
+        else:
+            logger.warning("webhook couldn't be disabled")
+
     def start_webhook(self):
-        from telegram import Bot
-        bot = Bot(token=config.TOKEN)  #try
-        self.dispatcher = Dispatcher(bot,None)
+        self.set_webhook()
+        self.dispatcher = Dispatcher(self.api,None) #TEMPORAL
         self.add_handlers()
 
-    def start_looping(self):
-        pass
+    def start_polling(self):
+        self.disable_webhook()
+        self.updater = Updater(token=config.TOKEN) # TEMPORAL
+        self.dispatcher = self.updater.dispatcher
+        self.add_handlers()
 
     def polling_loop(self):
-        pass
+
+        # Fix Python 2.x. input
+        try: input = raw_input
+        except NameError: pass
+
+        self.update_queue = self.updater.start_polling()
+        while True:
+            text = input("Write {stop} for stopping the bot\n")
+            if text == 'stop':
+                self.updater.stop()
+                break
+            elif len(text) > 0:
+                self.update_queue.put(text)
 
     def add_handlers(self):
+        self.dispatcher.addTelegramCommandHandler("start", self.command_start)
         self.dispatcher.addTelegramCommandHandler("help", self.command_help)
+        self.dispatcher.addTelegramCommandHandler("terraria", self.command_terraria)
+        self.dispatcher.addTelegramCommandHandler("list", self.command_list)
+        self.dispatcher.addTelegramCommandHandler("search", self.command_search)
+        self.dispatcher.addTelegramCommandHandler("settings",self.command_settings)
+        self.dispatcher.addUnknownTelegramCommandHandler(self.command_unknown)
+        #self.dispatcher.addErrorHandler(self.error_handle)
+
+    def command_start(self, bot, update):
+        bot.sendMessage(chat_id=update.message.chat_id, text=_("Bot was initiated. Use /help for commands."))
 
     def command_help(self, bot, update):
         bot.sendMessage(chat_id=update.message.chat_id, text=_(
@@ -129,4 +101,41 @@ class TelegramBot(object):
             /terraria status/autonot/ip - Terraria options
             /list <option> <item> - Manage your lists.
             /search <engine> <word> - Search using a engine."""))
+
+    def command_terraria(self, bot, update):
+        bot.sendMessage(chat_id=update.message.chat_id, text=_(
+            "/terraria status\n"
+            "/terraria autonot\n"
+            "/terraria ip\n"))
+
+    def command_list(self, bot, update):
+        bot.sendMessage(chat_id=update.message.chat_id, text=_("/list <option> <item>"))
+
+    def command_search(self, bot, update):
+        bot.sendMessage(chat_id=update.message.chat_id, text=_("/search <engine> <word>"))
+
+    def command_settings(self, bot,update):
+        languages_codes_text = _("Language codes:\n")
+        for lang in self.language_list:
+            languages_codes_text+= "<"+lang + "> "
+
+        help_text = _("Use /settings language language_code\n\n" + languages_codes_text)
+
+        command_args = update.message.text.split()
+        if len(command_args) < 3:
+            bot.sendMessage(chat_id=update.message.chat_id, text=help_text)
+        else:
+            if command_args[1] == "language" or "l":
+                if command_args[2] in self.language_list:
+                    bot.sendMessage(chat_id=update.message.chat_id, text=_("Language changed to %s") % (command_args[2]))
+                    config.LANGUAGE =  command_args[2]
+                    translation_install(self.translations[config.LANGUAGE])
+                else:
+                    bot.sendMessage(chat_id=update.message.chat_id, text=_("Unknown language code\n\n" + languages_codes_text))
+            else:
+                bot.sendMessage(chat_id=update.message.chat_id, text=help_text)
+
+    def command_unknown(self, bot, update):
+        bot.sendMessage(chat_id=update.message.chat_id, text=_("%s is a unknown command. Use /help for available commands.") % (update.message.text))
+
 
